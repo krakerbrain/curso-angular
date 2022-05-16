f8efe2abdbecbbbe0f60b242b6b20de14585e320
krakerbrain <marioplantabaja@gmail.com>
2022-05-15 20:11:17 -0400

    Primer Commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent {
+  heroes: string[] = [
+    'Spiderman',
+    'Ironman',
+    'Hulk',
+    'Thor',
+    'Capitán America',
+  ];
+
+  heroeBorrado: string = '';
+
+  borrarHeroe() {
+    const heroeBorrado = this.heroes.shift() || '';
+    this.heroeBorrado = heroeBorrado;
+  }
+}
